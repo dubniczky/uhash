c5528b4604add1b2c3d2a19226bbc30e3c745561
Richard Antal Nagy <nagy.richard.antal@gmail.com>
2023-03-10 22:39:08 +0100

    Added hash methods

--- hash.go
+++ hash.go
@@ -1,18 +1,24 @@
 package main
 
 import (
+	"crypto/md5"
 	"crypto/sha1"
-	"fmt"
+	"crypto/sha256"
+	"crypto/sha512"
+	"strings"
 )
 
 func protocolSwitcher(method string) func([]byte) []byte {
+	method = strings.ToLower(method)
     switch method {
-        case "sha1":
+        case "sha1", "sha-1":
             return hashSha1
-        case "sha256":
-            fmt.Println("sha256")
-        case "sha512":
-            fmt.Println("sha512")
+        case "sha256", "sha-256":
+            return hashSha256
+        case "sha512", "sha-512":
+            return hashSha512
+        case "md5":
+            return hashMd5
     }
     return nil
 }
@@ -20,5 +26,23 @@ func protocolSwitcher(method string) func([]byte) []byte {
 func hashSha1(data []byte) []byte {
     h := sha1.New()
     h.Write(data)
+	return h.Sum(nil)
+}
+
+func hashSha256(data []byte) []byte {
+	h := sha256.New()
+	h.Write(data)
+	return h.Sum(nil)
+}
+
+func hashSha512(data []byte) []byte {
+	h := sha512.New()
+	h.Write(data)
+	return h.Sum(nil)
+}
+
+func hashMd5(data []byte) []byte {
+	h := md5.New()
+	h.Write(data)
 	return h.Sum(nil)
 }
