df6c2d0bb02e86ef68a7394dd7d0201e37f25125
Richard Antal Nagy <nagy.richard.antal@gmail.com>
2023-03-19 19:03:47 +0100

    Merged code to one file

--- main.go
+++ main.go
@@ -4,10 +4,18 @@ import (
 	"bufio"
 	"errors"
 	"fmt"
-	"hash"
 	"io"
 	"log"
 	"os"
+	"strings"
+
+	"crypto/md5"
+	"crypto/sha1"
+	"crypto/sha256"
+	"crypto/sha512"
+	"hash"
+
+	"github.com/jzelinskie/whirlpool"
 )
 
 func stderr(msg string) {
@@ -16,6 +24,27 @@ func stderr(msg string) {
 
 func encodeHex(data []byte) string {
 	return fmt.Sprintf("%x", data)
+}
+
+func protocolSwitcher(method string) hash.Hash {
+	method = strings.ToLower(method)
+    switch method {
+        case "sha1", "sha-1":
+            return sha1.New()
+		case "sha224", "sha-224":
+			return sha512.New512_224()
+        case "sha256", "sha-256":
+            return sha256.New()
+		case "sha384", "sha-384":
+			return sha512.New384()
+        case "sha512", "sha-512":
+            return sha512.New()
+		case "whirlpool":
+			return whirlpool.New()
+        case "md5":
+            return md5.New()
+    }
+    return nil
 }
 
 func hashBuffer(r *bufio.Reader , hash hash.Hash) []byte {
